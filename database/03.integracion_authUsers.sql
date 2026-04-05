8e5b5820dad87cce46ae2110d68061e2c360530b
Alvaro Medina <alvarofmedinar@gmail.com>
2026-04-05 11:35:42 +0200

    Add auth_id column and user handling function
    Added auth_id column to usuarios table and created a function to handle new users.

--- database/03.integracion_authUsers.sql
+++ database/03.integracion_authUsers.sql
@@ -2,3 +2,17 @@
 
 ALTER TABLE usuarios
 ADD COLUMN auth_id UUID UNIQUE;
+
+-- Funcion para relacionar nombre (en tabla usuarios) con display_name (en tabla users) 
+create or replace function public.handle_new_user()
+returns trigger as $$
+begin
+  insert into public.usuarios (id, email, nombre)
+  values (
+    new.id,
+    new.email,
+    new.raw_user_meta_data->>'display_name'
+  );
+  return new;
+end;
+$$ language plpgsql security definer;
